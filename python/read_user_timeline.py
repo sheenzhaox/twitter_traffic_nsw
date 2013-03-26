1da06a6a1486188c266ceea9389d91a2d22a24a0
Xin Zhao <sheenzhaox@gmail.com>
2013-03-26 23:02:26 +1100

    modified:   read_user_timeline.py

--- python/read_user_timeline.py
+++ python/read_user_timeline.py
@@ -16,14 +16,24 @@ user = "TrafficNSW"
 # create twitter API object
 twitter = Twitter()
 
+# obtain current time
+current_time = datetime.now()
+print current_time
+
 # query the user timeline
 # twitter API docs: https://dev.twitter.com/docs/api/1/get/statuses/user_timeline
 results = twitter.statuses.user_timeline(screen_name = user)
 
-print results[0]
-print "(%s %s) %s" % (results[0]["created_at"], results[0]["user"]["time_zone"], results[0]["text"])
+# print results[0]
+# print "(%s %s) %s" % (results[0]["created_at"], results[0]["user"]["time_zone"], results[0]["text"])
 
 # loop through each of my statuses, and print its content
-# for status in results:
-#     # print "(%s) %s" % (status["created_at"], status["text"])
-#     print status
+for status in results:
+    # print "(%s) %s" % (status["created_at"], status["text"])
+    # print status
+    date_string = status["created_at"].split()
+    for i in date_string:
+        date_string[date_string.index(i)] = i.encode("utf-8")
+    data_string = status["text"]
+    print date_string
+    print " : " + data_string
