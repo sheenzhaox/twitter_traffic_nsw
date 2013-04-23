f87dadc6e9b75b3b8f91314e3422b942642e76ee
Xin Zhao <sheenzhaox@gmail.com>
2013-04-23 22:09:04 +1000

    1

---
+++ python/traffic_twitter_eclipse/src/testParse_trafficnsw_twitter_entry.py
@@ -0,0 +1,11 @@
+# test_parse_trafficnsw_twitter_entry.py
+
+import TrafficNSW
+
+tnsw = TrafficNSW.TrafficNSW()
+
+print str(tnsw)
+
+print tnsw.get_traffic_in_minutes(10)
+
+# print str(tnsw)
---
+++ python/traffic_twitter_eclipse/src/test_trafficNSW.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import TrafficNSW
+
+tnsw = TrafficNSW.TrafficNSW()
+
+# tnsw.obtain_traffic_from_twitter()
+# tnsw.obtain_traffic_from_twitter()
+
+# tnsw.get_latest_event()
+tnsw.print_events()
